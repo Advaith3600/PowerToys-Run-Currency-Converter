90b5195cb1ea2869b7e0fb2b978df007378f1c1d
Mikael Ribeiro Simoes <mikaelrsimoes19@gmail.com>
2024-06-10 17:58:31 +0400

    wip: adding get conversion into the test

---
+++ Community.PowerToys.Run.Plugin.CurrencyConverter.UnitTesting/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using System.Reflection;
+
+namespace Community.PowerToys.Run.Plugin.CurrencyConverter.UnitTesting
+{
+    public class GetConversionTest
+    {
+        [Fact]
+        public void GetConversion_ReturnsExpectedResult()
+        {
+            Main main = new Main();
+            MethodInfo method = main.GetType().GetMethod("GetConversion",  BindingFlags.NonPublic | BindingFlags.Instance);
+
+            object[] _params = new object[] { 2.0, "USD", "AED" };
+
+            var conversionResult = method.Invoke(main, _params);
+
+            Assert()
+        }
+    }
+}
